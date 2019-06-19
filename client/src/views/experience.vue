1ddeffd21b870b5283fc978068d9d0575e2d15a2
tristanlemoigne <tristanlemoigne@gmail.com>
2019-06-19 18:58:11 +0200

    Meree

--- client/src/views/experience.vue
+++ client/src/views/experience.vue
@@ -185,7 +185,7 @@
             <div class="globalEnd textGlow" v-bind:class="{ visible: canShowUIGlobaleEnd }">
                 <p v-bind:class="{ visible: !plurielMerged }">
                     Malheureusement,
-                    <br>votre collaboration n’a pas été suffisante<br/> pour reconstruire Pluriel
+                    <br>votre collaboration n’a pas été suffisante<br/>pour reconstruire Pluriel
                     <!-- Malheureusement,
                     <br>Votre collaboration n’a pas été suffisante -->
                 </p>
@@ -370,15 +370,14 @@ export default {
                 victoriousPlayer = "loose";
             }
 
-            threeBus.$emit("light up islands", victoriousPlayer);
             return victoriousPlayer;
         },
         getVictoriousIcon() {
             return `assets/img/${this.iconSrc}`;
         },
-        restartExperience() {
-            console.log("Restart xp");
-            this.setRoomState({ currentStep: { name: "trial_1_intro" } });
+        restartExperience(){
+            console.log("Restart xp")
+            this.setRoomState({currentStep: { name: 'trial_1_intro' }})
         }
     },
     watch: {
@@ -387,6 +386,7 @@ export default {
                 // Show good UI relative to current step
                 // UI RECAP TUTO
                 if (currentRoomState.currentStep.name === "trial_1_intro") {
+                    
                     // this.canShowUIGlobale = false;
 
                     this.canShowUIGlobale = false;
@@ -395,9 +395,9 @@ export default {
                     this.canShowUIEnd = false;
                     this.canShowUIGlobaleEnd = false;
 
-                    setTimeout(() => {
-                        this.audioFunctions.transitionNuages();
-                    }, 4000);
+                    setTimeout(()=>{
+                        this.audioFunctions.transitionNuages()
+                    }, 4000)
 
                     setTimeout(() => {
                         // this.canShowUITuto = true;
